ef0f21098e52d1b3b03a4a993b472fa0f82d9a7b
Ricky <rpadr014@fiu.edu>
2022-05-25 18:49:12 -0400

    added base; groupbox and listview

--- HW2/Form1.Designer.cs
+++ HW2/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(984, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Name";
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(984, 461);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // HW1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
             this.Location = new System.Drawing.Point(1000, 1000);
             this.Name = "HW1";
             this.Text = "HW1";
@@ -44,5 +67,8 @@
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private ListView listView1;
     }
 }
